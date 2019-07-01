0b6d026b19f54b831e25b26853ad01ece5a2f2e8
姚凯 <1294984305@qq.com>
2019-07-01 14:43:20 +0800

    user修改

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ class UserController extends Controller
     public function index()
     {
         $data = DB::select('select * from users');
+        echo '<pre>';
         var_dump($data);
     }
 }
